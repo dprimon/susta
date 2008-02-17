11169859400e9e1c9f967830c9197ce3f75e9159
dprimon <dprimon@8e29dbe9-b226-0410-8cf6-c0f5bd4410d5>
2008-02-17 11:05:12 +0000

    r7@bee:  daniele | 2008-02-14 18:49:57 +0100  repertorio locale di modifica  r8@bee:  root | 2008-02-15 11:14:22 +0100
     Some more explanation for the first approachers

--- susta/trunk/include/config.inc.php
+++ susta/trunk/include/config.inc.php
@@ -7,23 +7,24 @@
  * @copyleft 2005-2006
  **/
  
-$cfg['place']  = "dev";
-$cfg['dbserver'] = "localhost";
-$cfg['dbuser']   = "dev";
-$cfg['dbpass']   = "123";
-$cfg['dbname']   = "susta";
+$cfg['place']  = "dev"; 	// this indicates the running version. Only for memo purpose
+$cfg['dbserver'] = "localhost";	 // database server
+$cfg['dbuser']   = "dev";	// database username
+$cfg['dbpass']   = "123";	// password belonging to username
+$cfg['dbname']   = "susta";	// database name
 
 $cfg['locale']['dsn']    = "mysql://{$cfg['dbuser']}:{$cfg['dbpass']}@{$cfg['dbserver']}/{$cfg['dbname']}";
-$cfg['root']             = "{$_SERVER['DOCUMENT_ROOT']}/personale";
-$cfg['include_path']     = "{$cfg['root']}/include";
-$cfg['sessionname']      = "susta";
+$cfg['root']             = "{$_SERVER['DOCUMENT_ROOT']}/susta";	// directory on the web server to point the browser to
+$cfg['include_path']     = "{$cfg['root']}/include";	// directory where include files and libraries resides
+$cfg['sessionname']      = "susta";	// name of the session. Useful if the app is to be integrated with another
 
-# Login
-$cfg['username']         = "susta";
+# Front end login and password
+$cfg['username']         = "susta";	
 $cfg['password']     	= "susta";
 
 # OpenOffice tmp dir
-$cfg['ooodocdir'] = "/var/www/susta/";	
+#$cfg['ooodocdir'] = "/tmp";
+$cfg['ooodocdir'] = "{$cfg['root']}/tmp";	
 
 
 ?>
